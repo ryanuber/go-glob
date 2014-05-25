17987de7af1bfd73694479ab98b4a3341535bc70
Ryan Uber <ru@ryanuber.com>
2014-05-25 14:53:09 -0700

    Commented source code, changed package to 'glob'

--- glob.go
+++ glob.go
@@ -1,19 +1,25 @@
-package main
+package glob
 
-import (
-	"fmt"
-	"strings"
-)
+import "strings"
 
+// The character which is treated like a glob
 const GLOB = "*"
 
+// Glob will test a string pattern, potentially containing globs, against a
+// subject string. The result is a simple true/false, determining whether or
+// not the glob pattern matched the subject text.
 func Glob(pattern, subj string) bool {
+	// Shortcut the entire function if the pattern provided was empty. An empty
+	// pattern can, however, match an empty subject.
+	if pattern == "" {
+		return subj == pattern
+	}
+
 	parts := strings.Split(pattern, GLOB)
 
 	switch len(parts) {
-	case 0:
-		return false
 	case 1:
+		// No globs in pattern, so test for equality
 		return subj == pattern
 	default:
 		leadingGlob := strings.HasPrefix(pattern, GLOB)
@@ -23,12 +29,7 @@ func Glob(pattern, subj string) bool {
 		for i, part := range parts {
 			switch {
 			case i == 0 && leadingGlob:
-				end--
 				continue
-			case i == 1 && leadingGlob:
-				if !strings.Contains(subj, part) {
-					return false
-				}
 			case i == end:
 				if len(subj) > 0 {
 					return trailingGlob || strings.HasSuffix(subj, part)
@@ -39,14 +40,13 @@ func Glob(pattern, subj string) bool {
 					return false
 				}
 			}
+
+			// Trim evaluated text from subj as we loop over the pattern.
 			idx := strings.Index(subj, part) + len(part)
 			subj = subj[idx:]
 		}
+
+		// All parts of the pattern matched
+		return true
 	}
-
-	return true
-}
-
-func main() {
-	fmt.Printf("%#v\n", Glob("this is*long* test*woo", "this is a long test-sentence with many globswoo"))
 }
