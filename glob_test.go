c7f6b874de9440d29fd8e686485cb6b39882890f
Ryan Uber <ru@ryanuber.com>
2014-05-25 15:19:30 -0700

    Completed initial tests

--- glob_test.go
+++ glob_test.go
@@ -25,18 +25,20 @@ func TestPatternWithoutGlobs(t *testing.T) {
 
 func TestGlob(t *testing.T) {
 	for _, pattern := range []string{
-		"*test",
-		"this*",
-		"*is *",
-		"*is*a*",
+		"*test",           // Leading glob
+		"this*",           // Trailing glob
+		"*is *",           // String in between two globs
+		"*is*a*",          // Lots of globs
+		"**test**",        // Double glob characters
+		"**is**a***test*", // Varying number of globs
 	} {
 		testGlobMatch(t, pattern, "this is a test")
 	}
 
 	for _, pattern := range []string{
-		"test*",
-		"*is",
-		"*no*",
+		"test*", // Implicit substring match should fail
+		"*is",   // Partial match should fail
+		"*no*",  // Globs without a match between them should fail
 	} {
 		testGlobNoMatch(t, pattern, "this is a test")
 	}
